a4401654f609ae665bf43f728f66b4d4df526c6a
Piyush Negi <negip7740@gmail.com>
2022-12-19 11:59:28 +0530

    comments added

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
+// Function to get the Random Dish
 function getRandomDish() {
     document.querySelector('.modal-container').style.display = 'none'
 
+    // Fetching the api
     axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
         .then((resp) => {
-            console.log(resp)
-            document.getElementById('random-dish').innerHTML = `
-        <div>
-            <img src="${resp.data.meals[0].strMealThumb}" alt="">
-        </div>
 
-        <div>
-            <h1>${resp.data.meals[0].strMeal}</h1>
-        </div>
-        `
+            // Creating the Random Dish Card and putting all the required values inside it
+            document.getElementById('random-dish').innerHTML = `
+                <div>
+                    <img src="${resp.data.meals[0].strMealThumb}" alt="">
+                </div>
+
+                <div>
+                    <h1>${resp.data.meals[0].strMeal}</h1>
+                </div>
+                `
+
+            // Onclick function for the Random Dish Card to display the Modal and show it's ingredients
             document.getElementById('random-dish').onclick = () => {
                 document.getElementById('food-img').setAttribute('src',`${resp.data.meals[0].strMealThumb}`)
+
                 var modal = document.getElementById('ing')
                 modal.innerHTML = ''
+
                 document.querySelector('.modal-container').style.display = 'flex'
+
                 for(i=1; i<=20; i++){
                     let ing = 'strIngredient' + i
                     if(resp.data.meals[0][ing] != '' || resp.data.meals[0][ing] != null){
@@ -28,19 +36,27 @@ function getRandomDish() {
         })
 }
 
+// Calling the Random dish function
 getRandomDish();
 
+// Getting the dishes by searching the categories
 function getDishByCategory() {
+
+    // Taking the value of the input of the user
     let inputValue = document.getElementById('search-category').value
+
     var searchByCategory = document.getElementById('searched-category')
     var dishByCategory = document.getElementById('dish-by-category')
 
+    // Using the input value fetching the required api
     axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${inputValue}`).then((resp) => {
+
+        // If-else condition to see whether the value input by the user is a valid category or not (as per the api)
         if(resp.data.meals){
-            console.log(resp)
+
+            // If the category is valid then all the dishes in that category will be shown
             data = ''
             resp.data.meals.forEach((e) => {
-                // console.log(e)
                 data += `
                 <div class="card-container" onclick="getIng(${(e.idMeal)})">
                     <div>
@@ -58,12 +74,14 @@ function getDishByCategory() {
             searchByCategory.innerHTML = `<p>Your Searched Category's Dishes -</p>`
         }
 
+        // else it will ask to enter a valid food category
         else{
             searchByCategory.innerHTML = 'Please enter a valid food category'
         }
     })
 }
 
+// function to get the ingredients of the Dishes in the searched section
 function getIng(id){
     axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     .then((resp) => {
@@ -73,13 +91,14 @@ function getIng(id){
         document.querySelector('.modal-container').style.display = 'flex'
         for(i=1; i<=20; i++){
             let ing = 'strIngredient' + i
-            if(resp.data.meals[0][ing] != '' || resp.data.meals[0][ing] != 'null'){
+            if(resp.data.meals[0][ing] != '' && resp.data.meals[0][ing] != null && resp.data.meals[0][ing] != 'null'){
                 modal.innerHTML += `<li>${resp.data.meals[0][ing]}</li>`
             }
         }
     })
 }
 
+// function to provide onclick to close the modal
 function closeModal() {
     document.querySelector('.modal-container').style.display = 'none'
 }
